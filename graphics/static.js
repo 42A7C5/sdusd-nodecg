c1b610bd514a2847b51218240adf73f27097e4fb
Samuel Sharp <samuelsharp4@gmail.com>
2024-09-10 10:59:03 -0700

    Initial commit

---
+++ graphics/static.js
@@ -0,0 +1,36 @@
+window.schools = {
+    'POLO': {
+        id: 'POLO',
+        name: 'Point Loma',
+        team: 'Pointers',
+        color: {
+            primary: '#830000',
+            secondary: '#550000'
+        }
+    },
+    'RABE': {
+        id: 'RABE',
+        name: 'Rancho Bernardo',
+        team: 'Broncos',
+        color: {
+            primary: '#2C328C',
+            secondary: '#23286D'
+        }
+    },
+    'SAND': {
+        id: 'SAND',
+        name: 'San Diego',
+        team: 'Cavers',
+        color: {
+            primary: '#0072BC',
+            secondary: '#005E9B'
+        }
+    }
+}
+
+window.game = {
+    away: 'RABE',
+    home: 'POLO',
+    anchorA: 'Ethan Ramirez',
+    anchorB: 'Hillel Jacobs'
+}
