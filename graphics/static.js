2788169d4278e2462dceb97479621032f1291a82
Samuel Sharp <samuelsharp4@gmail.com>
2024-09-25 11:16:13 -0700

    Key players

--- graphics/static.js
+++ graphics/static.js
@@ -51,5 +51,13 @@ window.game = {
     home: 'KEAR',
     anchorA: 'Kennedy Murphy',
     anchorB: 'Hillel Jacobs',
-    keys: ['Play fast', 'Play smart', 'Play together']
+    lowers: [
+        [0, 'Keys to the game', ['Play fast', 'Play smart', 'Play together']],
+        [1, '#1 Caver Bob', ['Is fast', 'Plays smart']],
+        [1, '#2 Caver Bob', ['Is fast', 'Plays smart']],
+        [1, '#3 Caver Bob', ['Is fast', 'Plays smart']],
+        [2, '#4 Caver Bob', ['Is fast', 'Plays smart']],
+        [2, '#5 Caver Bob', ['Is fast', 'Plays smart']],
+        [2, '#6 Caver Bob', ['Is fast', 'Plays smart']],
+    ]
 }
